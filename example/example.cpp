8febadb3df0b705ced89546bd7fafd39624a1a3a
James A. Chappell <rlrrlrll@gmail.com>
2023-04-21 15:32:11 -0500

    clean up example

--- example/example.cpp
+++ example/example.cpp
@@ -11,11 +11,11 @@ int main()
   auto num = 10u;
 
   auto die = Random<unsigned short>(1, 6);
-  std::cout << "Simulate " << num <<  " dice (die) rolls:" << std::endl;
+  std::cout << "Simulate " << num <<  " dice (die) rolls:" << '\n';
   for (auto i = 0u ; i < num ; ++i)
   {
     std::cout << "Roll " << std::setfill(' ') << std::setw(2) << i + 1  << ": "
-              << die() << std::endl;
+              << die() << '\n';
   }
 
   auto min = -1.0;
@@ -23,7 +23,7 @@ int main()
 
   auto ran = Random<double>(min, max);
   std::cout << "\nGenerate " << num << " random numbers between "
-            << min << " and " << max << ":" << std::endl;
+            << min << " and " << max << ":\n";
 
   for (auto i = 0u ; i < num ; ++i)
   {
